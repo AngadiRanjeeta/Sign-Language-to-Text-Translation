8db19f3ead4d935bc62092b41e5ad2aad312c0d3
Ranjeeta Angadi <145282021+AngadiRanjeeta@users.noreply.github.com>
2024-05-14 23:21:08 +0530

    Update attention_model.py

--- attention_model.py
+++ attention_model.py
@@ -1,18 +1,4 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Attention-based sequence-to-sequence model with dynamic RNN support."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -26,14 +12,7 @@ __all__ = ["AttentionModel"]
 
 
 class AttentionModel(model.Model):
-    """Sequence-to-sequence dynamic model with attention.
-
-    This class implements a multi-layer recurrent neural network as encoder,
-    and an attention-based decoder. This is the same as the model described in
-    (Luong et al., EMNLP'2015) paper: https://arxiv.org/pdf/1508.04025v5.pdf.
-    This class also allows to use GRU cells in addition to LSTM cells with
-    support for dropout.
-    """
+   
 
     def __init__(self, hparams, mode, iterator,
                  target_vocab_table, reverse_target_vocab_table=None,
